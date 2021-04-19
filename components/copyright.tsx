17eee2f993c784d2b6cab1ccf0805ca610dfc999
mward-sudo <81108541+mward-sudo@users.noreply.github.com>
2021-04-19 16:29:32 +0100

    Copyright footer formatting

--- components/copyright.tsx
+++ components/copyright.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 export default function Copyright() {
+  const useStyles = makeStyles(() => ({
+    copyright: {
+      marginTop: "3em",
+      fontSize: 10
+    }
+  }));
+  const classes = useStyles();
+
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      Copyright © Michael Ward {new Date().getFullYear()}.
+    <Typography
+      variant="body2"
+      className={classes.copyright}
+      color="textSecondary"
+      align="center"
+    >
+      &copy; Copyright Michael Ward {new Date().getFullYear()}
     </Typography>
   );
 }
