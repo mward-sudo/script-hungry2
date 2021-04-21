3f15285e3583443f3fcf166fb4c3860452e7fc56
Michael Ward <mward82@gmail.com>
2021-04-21 22:14:57 +0000

    Header linted

--- components/header.tsx
+++ components/header.tsx
@@ -1,52 +1,50 @@
-import { FC, ElementType } from "react";
-import Link from "next/link";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { ElementType, FC } from 'react'
+import Link from 'next/link'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import {
   AppBar,
   Toolbar,
   Typography,
   IconButton,
-  Container
-} from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
+  Container,
+} from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
 
 type HeaderProps = {
-  element?: ElementType;
+  element?: ElementType
 };
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-      marginBottom: "6em"
-    },
-    appBar: {
-      display: "sticky",
-      backgroundColor: "rgba(255,255,255,0.75)",
-      backdropFilter: "blur(6px)",
-      zIndex: 1000
-    },
-    menuButton: {
-      marginRight: theme.spacing(2)
-    },
-    title: {
-      fontFamily: "monospace",
-      fontSize: 20,
-      flexGrow: 1,
-      color: "#333"
-    },
-    homeLink: {
-      color: "inherit",
-      textDecoration: "inherit"
-    },
-    toolbar: {
-      padding: 0
-    }
-  })
-);
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  root: {
+    flexGrow: 1,
+    marginBottom: '6em',
+  },
+  appBar: {
+    display: 'sticky',
+    backgroundColor: 'rgba(255,255,255,0.75)',
+    backdropFilter: 'blur(6px)',
+    zIndex: 1000,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    fontFamily: 'monospace',
+    fontSize: 20,
+    flexGrow: 1,
+    color: '#333',
+  },
+  homeLink: {
+    color: 'inherit',
+    textDecoration: 'inherit',
+  },
+  toolbar: {
+    padding: 0,
+  },
+}))
 
-const Header: FC<HeaderProps> = ({ element = "h1" }) => {
-  const classes = useStyles();
+const Header: FC<HeaderProps> = ({ element = 'h1' }) => {
+  const classes = useStyles()
 
   return (
     <div className={classes.root}>
@@ -74,7 +72,7 @@ const Header: FC<HeaderProps> = ({ element = "h1" }) => {
         </Container>
       </AppBar>
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
