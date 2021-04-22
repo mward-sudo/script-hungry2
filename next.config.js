be3ff9a879d11dcf42ae27ab116ec0f02bd99934
Michael Ward <mward82@gmail.com>
2021-04-22 12:22:50 +0000

    Tedt strict TypeScript building

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   images: {
     domains: [
-      "res.cloudinary.com",
-      "secure.gravatar.com",
-      "scripthungry.cloudaccess.host"
-    ]
+      'res.cloudinary.com',
+      'secure.gravatar.com',
+      'scripthungry.cloudaccess.host',
+    ],
   },
   future: {
-    webpack5: true
+    webpack5: true,
   },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
-    ignoreBuildErrors: true
-  }
-};
+    // ignoreBuildErrors: true
+  },
+}
