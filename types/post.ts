ac434c61c6da4c2996368bf3a19f26efdecefe5d
Michael Ward <mward82@gmail.com>
2021-04-22 01:09:37 +0000

    Adds optional properties to Post interface

--- types/post.ts
+++ types/post.ts
@@ -2,8 +2,10 @@ import iFeaturedImage from './featured-image'
 import iAuthor from './author'
 
 export default interface iPost {
+  id?: string;
   title: string;
   excerpt: string;
+  content?: string;
   slug: string;
   date: string;
   featuredImage: iFeaturedImage;
