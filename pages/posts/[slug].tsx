9ee93776d717b7cd52fcb11a44dc392916bd24da
Michael Ward <mward82@gmail.com>
2021-04-22 01:10:33 +0000

    Lints posts/[slug].tsx

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,30 +1,39 @@
-import { useState } from "react";
-import { Box, Container, Button } from "@material-ui/core";
-import Head from "next/head";
-import dynamic from "next/dynamic";
-import Copyright from "../../components/copyright";
-import Header from "../../components/header";
-import PostHeaderWithImage from "../../components/post-header-with-image";
-import PostHeader from "../../components/post-header";
-import { getAllPostsWithSlug, getPost } from "../../lib/api";
-import Constants from "../../lib/consts";
-// import Disqus from "../../components/disqus";
-import { useRouter } from "next/router";
+import { FC, useState } from 'react'
+import { Box, Container, Button } from '@material-ui/core'
+import { GetStaticProps, GetStaticPaths } from 'next'
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
+import { useRouter } from 'next/router'
+import Copyright from '../../components/copyright'
+import Header from '../../components/header'
+import PostHeaderWithImage from '../../components/post-header-with-image'
+import PostHeader from '../../components/post-header'
+import { getAllPostsWithSlug, getPost } from '../../lib/api'
+import Constants from '../../lib/consts'
+import iPost from '../../types/post'
 
-const Disqus = dynamic(() => import("../../components/disqus"), {
-  loading: () => <p>...</p>
-});
+const Disqus = dynamic(() => import('../../components/disqus'), {
+  loading: () => <p>...</p>,
+})
 
-const Post = ({ post, comments = false, preview }) => {
-  const [showComments, setShowComments] = useState(false);
+type PostProps = {
+  post: iPost
+}
 
-  const router = useRouter();
+const Post: FC<PostProps> = ({ post }) => {
+  const [showComments, setShowComments] = useState(false)
+
+  const router = useRouter()
 
   return (
     <>
       <Head>
         <title>
-          {post?.title} | {Constants.SITE_NAME}
+          {post?.title}
+          {' '}
+          |
+          {' '}
+          {Constants.SITE_NAME}
         </title>
       </Head>
       <Header element="p" />
@@ -38,13 +47,13 @@ const Post = ({ post, comments = false, preview }) => {
           ) : (
             <PostHeader title={post?.title} />
           )}
-          <div dangerouslySetInnerHTML={{ __html: post?.content }}></div>
+          <div dangerouslySetInnerHTML={{ __html: post?.content }} />
           {showComments ? (
             <Disqus
               key={post?.id}
               pageTitle={post?.title}
               pageID={post?.id}
-              pageURL={`https://scripthungr2.vercel.app${router.asPath}`}
+              pageURL={`https://scripthungry.com${router.asPath}`}
             />
           ) : (
             <Button
@@ -60,28 +69,28 @@ const Post = ({ post, comments = false, preview }) => {
         </Box>
       </Container>
     </>
-  );
-};
+  )
+}
 
-export async function getStaticProps({ params, preview = false, previewData }) {
-  const data = await getPost(params.slug, preview, previewData);
+export const getStaticProps: GetStaticProps = async ({ params, preview = false, previewData }) => {
+  const data = await getPost(params.slug, preview, previewData)
 
   return {
     props: {
       preview,
-      post: data.post
+      post: data.post,
     },
-    revalidate: 60
-  };
+    revalidate: 60,
+  }
 }
 
-export async function getStaticPaths() {
-  const allPosts = await getAllPostsWithSlug();
+export const getStaticPaths: GetStaticPaths = async () => {
+  const allPosts = await getAllPostsWithSlug()
 
   return {
     paths: allPosts.edges.map(({ node }) => `/posts/${node.slug}`) || [],
-    fallback: true
-  };
+    fallback: true,
+  }
 }
 
-export default Post;
+export default Post
