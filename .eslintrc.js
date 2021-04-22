958b83cab903900881ce2ca60db2ec7c0256b306
Michael Ward <mward82@gmail.com>
2021-04-22 00:31:13 +0000

    Removes prettier from eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
     'plugin:react/recommended',
     'airbnb',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
