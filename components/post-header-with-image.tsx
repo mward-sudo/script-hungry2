5682c4f26bd4775b13a5a3b9e9d904ce3830bcf0
Michael Ward <mward82@gmail.com>
2021-04-22 00:36:26 +0000

    Lints post-header-image and renames from .jsx to .tsx

---
+++ components/post-header-with-image.tsx
@@ -0,0 +1,46 @@
+import { FC } from 'react'
+import { Box, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import PostHeaderImage from './post-header-image'
+import iFeaturedImage from '../types/featured-image'
+
+type PostHeaderWithImageProps = {
+  title: string
+  image: iFeaturedImage
+}
+
+const PostHeaderWithImage: FC<PostHeaderWithImageProps> = ({ title, image }) => {
+  const useStyles = makeStyles(() => ({
+    positionRoot: {
+      position: 'relative',
+    },
+    heading: {
+      position: 'absolute',
+      width: '50%',
+      top: '1em',
+      left: '1em',
+      zIndex: 100,
+    },
+    headingSpan: {
+      backgroundColor: 'red',
+      color: '#fff',
+      lineHeight: '1.4',
+    },
+  }))
+  const classes = useStyles()
+
+  return (
+    <Box className={classes.positionRoot}>
+      <Typography variant="h5" component="h1" className={classes.heading}>
+        <span className={classes.headingSpan}>{title}</span>
+      </Typography>
+      <PostHeaderImage
+        url={image.node.sourceUrl}
+        height={image.node.mediaDetails?.height}
+        width={image.node.mediaDetails?.width}
+      />
+    </Box>
+  )
+}
+
+export default PostHeaderWithImage
