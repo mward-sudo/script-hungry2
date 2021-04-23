09eb82136efb5e607b7c766815bb09a3e0c93f73
Michael Ward <mward82@gmail.com>
2021-04-23 14:28:00 +0000

    On iPost interface, id made mandatory and extend content iface added

--- types/post.ts
+++ types/post.ts
@@ -2,14 +2,17 @@ import iFeaturedImage from './featured-image'
 import iAuthor from './author'
 
 export default interface iPost {
-  id?: string;
+  id: string;
   title: string;
   excerpt: string;
-  content?: string;
   slug: string;
   date: string;
   featuredImage: iFeaturedImage;
   author: {
     node: iAuthor;
   }
+}
+
+export interface iPostWithContent extends iPost {
+  content: string;
 }
