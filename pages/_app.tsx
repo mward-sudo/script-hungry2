8a3bf1ffac8cb0bde4b36254772d404732465ecb
Michael Ward <mward82@gmail.com>
2021-04-23 13:33:28 +0000

    Fixes object might be null error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,9 +11,7 @@ const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side')
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles)
-    }
+    jssStyles?.parentElement?.removeChild(jssStyles)
   }, [])
 
   return (
