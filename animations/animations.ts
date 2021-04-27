ea12ce400c1752b328a3af29df35cd9a64c835b4
Michael Ward <mward82@gmail.com>
2021-04-27 00:00:29 +0000

    Adds zoom animation

--- animations/animations.ts
+++ animations/animations.ts
@@ -1,5 +1,6 @@
 import { AnimationProps } from 'framer-motion'
 import { Fade } from '../types/fade'
+import { Zoom } from '../types/zoom'
 
 export const stagger = (staggerTime = 0.2): AnimationProps => ({
   animate: {
@@ -32,3 +33,12 @@ export const fadeIn: Fade = (durationLength = 0.5, delayLength = 0) =>
 
 export const fadeOut: Fade = (durationLength = 0.5, delayLength = 0) =>
   fade(durationLength, delayLength, 1, 0)
+
+export const zoom: Zoom = (zoomAmount = 1.1) => ({
+  initial: {
+    zoom: 1,
+  },
+  animate: {
+    zoom: zoomAmount,
+  },
+})
---
+++ types/zoom.ts
@@ -0,0 +1,5 @@
+import { Variants } from 'framer-motion'
+
+export interface Zoom {
+  (zoomAmount: number): Variants
+}
