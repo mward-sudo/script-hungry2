f4a0661e886fe3e3dbae94dd81c411c2f74a8897
Michael Ward <mward82@gmail.com>
2021-04-27 21:55:53 +0000

    Added some jsdoc

--- animations/animations.ts
+++ animations/animations.ts
@@ -9,12 +9,30 @@ import {
   ZoomParams,
 } from '../types/animations'
 
+/**
+ * Default parameters of the stagger function
+ * @constant staggerDefaultProps
+ * @type StaggerParams
+ */
 const staggerDefaultProps: StaggerParams = {
   staggerTime: 0.2,
 }
+
+/**
+ * Allows framer-motion to stagger the animation of child elemenets.
+ * Returns an obejct of type Variants which should be passed to the
+ * variants property of a framer-motion motion tag.
+ * @param params @type StaggerParams
+ * @returns Variants
+ */
 export const stagger: Stagger = (params) => {
   // Spreading the default props and the params gives us an object with
   // default values, overridden by any that were passed in via params
+
+  /**
+   * The amount of time between each child animation starting in the stagger
+   * @constant staggerTime number
+   */
   const { staggerTime } = { ...staggerDefaultProps, ...params }
 
   return {
@@ -26,12 +44,24 @@ export const stagger: Stagger = (params) => {
   }
 }
 
+/**
+ * The default properties for the fade function
+ * @constant fadeDefaultProps
+ * @returns FadeParams
+ */
 const fadeDefaultProps: FadeParams = {
   initialOpacity: 0,
   animateOpacity: 1,
   durationLength: 0.5,
   delayLength: 0,
 }
+/**
+ * Non-exported utility function for fade functionality.
+ * Returns an obejct of type Variants which should be passed to the
+ * variants property of a framer-motion motion tag.
+ * @param params FadeParams
+ * @returns Variants
+ */
 const fade: Fade = (params) => {
   // Spreading the default props and the params gives us an object with
   // default values, overridden by any that were passed in via params
