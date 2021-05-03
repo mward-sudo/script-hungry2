e1ccc36c8775b888b708ca9d64f0bee444882ce1
Michael Ward <mward82@gmail.com>
2021-05-03 13:36:12 +0000

    Removes unneccesary webpack / future config in next.config.js

--- next.config.js
+++ next.config.js
@@ -8,9 +8,6 @@ module.exports = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     quality: 25,
   },
-  future: {
-    webpack5: true,
-  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
