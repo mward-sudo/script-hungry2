c270851196ad54bafaad3e01b159ff037cba0db7
Michael Ward <mward82@gmail.com>
2021-07-15 21:55:17 +0100

    Replaces home page hero image with css gradient

--- components/home/intro.tsx
+++ components/home/intro.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react'
 import { motion } from 'framer-motion'
-import Image from 'next/image'
 import { fadeInAndUp, stagger } from '@/animations/animations'
 import { HomePageHero } from '@/types/graphcms-api'
 
@@ -15,11 +14,14 @@ const HomeIntro: FC<HomeIntroProps> = ({ homePageHero }) => {
       className="relative bg-black overflow-hidden mb-8"
       style={{ maxHeight: '60vh' }}
     >
-      <Image
-        src={homePageHero.backgroundImage.url}
-        width={homePageHero.backgroundImage.width}
-        height={homePageHero.backgroundImage.height}
-        layout="responsive"
+      <div
+        style={{
+          background:
+            'linear-gradient(115deg, rgb(211, 255, 215) 0%, rgb(0, 0, 0) 100%), radial-gradient(90% 100% at 50% 0%, rgb(200, 200, 200) 0%, rgb(22, 0, 45) 100%), radial-gradient(100% 100% at 80% 0%, rgb(250, 255, 0) 0%, rgb(36, 0, 0) 100%), radial-gradient(150% 210% at 100% 0%, rgb(112, 255, 0) 0%, rgb(20, 175, 125) 0%, rgb(0, 10, 255) 100%), radial-gradient(100% 100% at 100% 30%, rgb(255, 77, 0) 0%, rgba(0, 200, 255, 1) 100%), linear-gradient(60deg, rgb(255, 0, 0) 0%, rgb(120, 86, 255) 100%)',
+          backgroundBlendMode:
+            'overlay, overlay, difference, difference, difference, normal',
+          height: '60vh',
+        }}
       />
 
       <motion.div
