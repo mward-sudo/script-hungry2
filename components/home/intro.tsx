aae940740e45c64cf2a34970e00f5cbbcb358ee5
Michael Ward <mward82@gmail.com>
2021-07-16 04:03:50 +0100

    Adds animation to home page hero

--- components/home/intro.tsx
+++ components/home/intro.tsx
@@ -14,15 +14,38 @@ const HomeIntro: FC<HomeIntroProps> = ({ homePageHero }) => {
       className="relative bg-black overflow-hidden mb-8"
       style={{ maxHeight: '60vh' }}
     >
-      <div
-        style={{
-          background:
-            'linear-gradient(115deg, rgb(211, 255, 215) 0%, rgb(0, 0, 0) 100%), radial-gradient(90% 100% at 50% 0%, rgb(200, 200, 200) 0%, rgb(22, 0, 45) 100%), radial-gradient(100% 100% at 80% 0%, rgb(250, 255, 0) 0%, rgb(36, 0, 0) 100%), radial-gradient(150% 210% at 100% 0%, rgb(112, 255, 0) 0%, rgb(20, 175, 125) 0%, rgb(0, 10, 255) 100%), radial-gradient(100% 100% at 100% 30%, rgb(255, 77, 0) 0%, rgba(0, 200, 255, 1) 100%), linear-gradient(60deg, rgb(255, 0, 0) 0%, rgb(120, 86, 255) 100%)',
-          backgroundBlendMode:
-            'overlay, overlay, difference, difference, difference, normal',
-          height: '60vh',
-        }}
-      />
+      <div className="overflow-hidden relative" style={{ height: '60vh' }}>
+        <motion.div
+          style={{
+            background:
+              'linear-gradient(115deg, rgb(211, 255, 215) 0%, rgb(0, 0, 0) 100%), radial-gradient(90% 100% at 50% 0%, rgb(200, 200, 200) 0%, rgb(22, 0, 45) 100%), radial-gradient(100% 100% at 80% 0%, rgb(250, 255, 0) 0%, rgb(36, 0, 0) 100%), radial-gradient(150% 210% at 100% 0%, rgb(112, 255, 0) 0%, rgb(20, 175, 125) 0%, rgb(0, 10, 255) 100%), radial-gradient(100% 100% at 100% 30%, rgb(255, 77, 0) 0%, rgba(0, 200, 255, 1) 100%), linear-gradient(60deg, rgb(255, 0, 0) 0%, rgb(120, 86, 255) 100%)',
+            backgroundBlendMode:
+              'overlay, overlay, difference, difference, difference, normal',
+            height: '120vh',
+            width: '200%',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+          }}
+          variants={{
+            animate: {
+              left: ['-100%', '0%', '-100%'],
+              top: ['-100%', '0%', '-100%', '0%', '-100%'],
+              transform: ['deg(0)', 'deg(360)'],
+              filter: [
+                'hue-rotate(0)',
+                'hue-rotate(360deg)',
+                'hue-rotate(0deg)',
+              ],
+              transition: {
+                duration: 20,
+                repeat: Infinity,
+              },
+            },
+          }}
+          animate="animate"
+        />
+      </div>
 
       <motion.div
         variants={stagger()}
