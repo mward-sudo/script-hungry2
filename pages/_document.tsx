bc002e56b42c35105dc31beb2774d3353f651479
Michael Ward <mward82@gmail.com>
2021-08-09 19:18:58 +0100

    Gray background colour

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
         </Head>
-        <body>
+        <body className="bg-gray-100">
           <Main />
           <NextScript />
         </body>
