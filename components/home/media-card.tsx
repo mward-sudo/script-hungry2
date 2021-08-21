39d7dc05ca913a312249b482d0162f60bb839a2f
mward-sudo <81108541+mward-sudo@users.noreply.github.com>
2021-08-21 17:49:23 +0100

    Remove fade in and up animations on home page cards

--- components/home/media-card.tsx
+++ components/home/media-card.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link'
 import useDimensions from 'react-cool-dimensions'
 import { FC } from 'react'
 import { m, Variants } from 'framer-motion'
-import { fadeInAndUp } from '@/animations/animations'
 
 const hoverImgVariant: Variants = {
   initial: {
@@ -43,37 +42,33 @@ const HomeMediaCard: FC<MediaCardProps> = ({
   const { observe, width } = useDimensions<HTMLDivElement | null>()
 
   return (
-    <m.div variants={fadeInAndUp()}>
-      <Link href={link}>
-        <a className="no-underline uppercase text-base">
-          <m.div
-            initial="initial"
-            whileHover="hover"
-            whileTap="tap"
-            className="border-2 border-solid border-gray-200 dark:border-gray-800 rounded-lg overflow-hidden drop-shadow-sm bg-white dark:bg-gray-900"
-          >
-            <div style={{ overflow: 'hidden' }}>
-              <m.div variants={hoverImgVariant} ref={observe}>
-                <Image
-                  layout="responsive"
-                  src={image}
-                  height={imageHeight}
-                  width={imageWidth}
-                  alt={imageAlt}
-                  sizes={
-                    width !== undefined ? `${Math.round(width)}px` : '100vw'
-                  }
-                  priority
-                />
-              </m.div>
-            </div>
-            <div className="text-black dark:text-gray-200 text-center no-underline py-3 px-2">
-              {linkText} &gt;
-            </div>
-          </m.div>
-        </a>
-      </Link>
-    </m.div>
+    <Link href={link}>
+      <a className="text-base no-underline uppercase">
+        <m.div
+          initial="initial"
+          whileHover="hover"
+          whileTap="tap"
+          className="overflow-hidden bg-white border-2 border-gray-200 border-solid rounded-lg dark:border-gray-800 drop-shadow-sm dark:bg-gray-900"
+        >
+          <div style={{ overflow: 'hidden' }}>
+            <m.div variants={hoverImgVariant} ref={observe}>
+              <Image
+                layout="responsive"
+                src={image}
+                height={imageHeight}
+                width={imageWidth}
+                alt={imageAlt}
+                sizes={width !== undefined ? `${Math.round(width)}px` : '100vw'}
+                priority
+              />
+            </m.div>
+          </div>
+          <div className="px-2 py-3 text-center text-black no-underline dark:text-gray-200">
+            {linkText} &gt;
+          </div>
+        </m.div>
+      </a>
+    </Link>
   )
 }
 
