1762ae1e01e8a0d201da1af87dc7d523bc35a677
mward-sudo <81108541+mward-sudo@users.noreply.github.com>
2021-08-23 19:27:06 +0100

    Reverts to react instead of preact due to framer-motion incompatability

--- next.config.js
+++ next.config.js
@@ -18,14 +18,6 @@ module.exports = withBundleAnalyzer({
       })
     )
 
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
-
     return config
   },
   images: {
